fc209c2968425d1c6d4e65cc56e986f043b1998d
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-12-31 14:01:41 +0100

    Semiautomatic update of documentation

---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["finitediff"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"finitediff/struct.PerturbationVector.html\" title=\"struct finitediff::PerturbationVector\">PerturbationVector</a>",synthetic:true,types:["finitediff::PerturbationVector"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["finitediff"] = {"name":"","dirs":[],"files":["lib.rs"]};
