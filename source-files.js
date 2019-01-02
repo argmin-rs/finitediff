2abfa33f3d0e5bfba1c290ca679480841880f56d
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-01-02 15:11:53 +0100

    Semiautomatic update of documentation

--- source-files.js
+++ source-files.js
@@ -1,2 +1,2 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["finitediff"] = {"name":"","dirs":[],"files":["diff.rs","hessian.rs","jacobian.rs","lib.rs","pert.rs"]};
+sourcesIndex["finitediff"] = {"name":"","dirs":[],"files":["diff.rs","hessian.rs","jacobian.rs","lib.rs","pert.rs","utils.rs"]};
