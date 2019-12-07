780bf3e3193f342f508a3dd99ed897f12676619f
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-12-07 13:04:24 +0100

    Semiautomatic update of documentation

--- implementors/finitediff/trait.FiniteDiff.js
+++ implementors/finitediff/trait.FiniteDiff.js
@@ -6,5 +6,4 @@ implementors["finitediff"] = [];
             } else {
                 window.pending_implementors = implementors;
             }
-        
-})()
+        })()
--- implementors/std/panic/trait.RefUnwindSafe.js
+++ implementors/std/panic/trait.RefUnwindSafe.js
@@ -6,5 +6,4 @@ implementors["finitediff"] = [{text:"impl <a class=\"trait\" href=\"https://doc.
             } else {
                 window.pending_implementors = implementors;
             }
-        
-})()
+        })()
